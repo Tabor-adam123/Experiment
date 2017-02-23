58b7afc967fb8a04e51cc3a1d77ec566a26ff826
risa2 <blazek.risa@gmail.com>
2017-02-23 19:29:00 +0100

    Editable fields

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def execute(cur, query, *args):
 con=sql.connect(':memory:')
 cur=con.cursor()
 
-cur.execute('CREATE TABLE List (Name, Number)')
+cur.execute('CREATE TABLE List (Id integer primary key autoincrement, Name, Number)')
 
 window=tk.Tk()
 
@@ -42,14 +42,24 @@ find_user_entry.grid(row=1, column=1)
 
 labels=[]
 
+def edit_field(var, column, id):
+    execute(cur, 'UPDATE List SET '+column+' = (?) WHERE Id = (?)', var.get(), id)
+
 def new_table(all):
     global labels
     for label in labels:
         label.grid_forget()
     labels=[]
-    for pair in all:
-        labels.append(tk.Label(window, text=pair[0], relief=tk.SUNKEN, anchor=tk.W))
-        labels.append(tk.Label(window, text=pair[1], relief=tk.SUNKEN, anchor=tk.W))
+    for row in all:
+        sv=tk.StringVar()
+        sv.set(row[1])
+        sv.trace('w', lambda x,y,z:edit_field(sv, 'Name', row[0]))
+        labels.append(tk.Entry(window, textvariable=sv, relief=tk.SUNKEN))
+        
+        sv2=tk.StringVar()
+        sv2.set(row[2])
+        sv2.trace('w', lambda x,y,z:edit_field(sv2, 'Number', row[0]))
+        labels.append(tk.Entry(window, textvariable=sv2, relief=tk.SUNKEN))
     grid_all_labels(labels, minrow=2, column=0, first_width=3, second_width=2)
 
 def refresh():
@@ -57,7 +67,7 @@ def refresh():
 
 def add_user_command():
     if add_user_name_var.get() and add_user_number_var.get():
-        execute(cur, 'INSERT INTO List VALUES (?,?)', add_user_name_var.get(), add_user_number_var.get())
+        execute(cur, 'INSERT INTO List (Name, Number) VALUES (?,?)', add_user_name_var.get(), add_user_number_var.get())
         add_user_name_var.set('')
         add_user_number_var.set('')
         refresh()
@@ -67,13 +77,13 @@ add_user_button.grid(row=0, column=4)
 
 def find_user_command():
     if find_user_var.get():
-        new_table([(find_user_var.get(), number) for number in execute(cur, 'SELECT Number FROM List WHERE Name=?', find_user_var.get()).fetchall()])
+        new_table(execute(cur, 'SELECT * FROM List WHERE Name=?', find_user_var.get()).fetchall())
         find_user_var.set('')
 
 find_user_button=tk.Button(window, text='Vyhledat uživatele', command=find_user_command)
 find_user_button.grid(row=1, column=2)
 
-refresh_button=tk.Button(window, text='Zpět na seznam', command=find_user_command)
+refresh_button=tk.Button(window, text='Zpět na seznam', command=refresh)
 refresh_button.grid(row=1, column=3, columnspan=2, sticky=tk.W+tk.E)
 
 window.mainloop()
